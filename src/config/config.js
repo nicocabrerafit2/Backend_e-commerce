3da3fcb0335f9cf41b457c3e608efb880507b482
Nicolás Damián Cabrera <106848580+nicocabrerafit2@users.noreply.github.com>
2024-12-15 21:56:33 -0300

    agrego primeros test

--- src/config/config.js
+++ src/config/config.js
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import { persistenceDaoInit } from "../persistence/factory.js";
 import { configPassport } from "../middlewares/jwtPassport.js";
 import passport from "passport";
@@ -14,5 +15,6 @@ export const AppInit = (app) => {
   app.use(cookieParser(process.env.SECRET));
   app.use(express.json());
   app.use(express.urlencoded({ extended: true }));
+  app.use(cors());
   app.use("/", routerIndex);
 };
